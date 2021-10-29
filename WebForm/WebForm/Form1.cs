d46a49011a5b8697a1eca3909dad5f09e2f49455
Sára Szabó <saraszabo999@gmail.com>
2021-10-29 10:13:45 +0200

    connect to database

--- WebForm/WebForm/Form1.cs
+++ WebForm/WebForm/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WebForm.MNBServiceReference;
 
 namespace WebForm
 {
@@ -15,6 +17,18 @@ namespace WebForm
         public Form1()
         {
             InitializeComponent();
+        }
+
+        void Consume()
+        {
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
+            request.currencyNames = "EUR";
+            request.startDate = "2020-01-01";
+            request.endDate = "2020-06-30";
+            var response = mnbService.GetExchangeRates(request);
+            string result = response.GetExchangeRatesResult;
+            File.WriteAllText("export.xml", result);
         }
     }
 }
