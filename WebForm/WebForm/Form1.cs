7e96eb9acdc8cc830443cac7e01dc7490c4c7b56
Sára Szabó <saraszabo999@gmail.com>
2021-10-29 10:24:18 +0200

    xml

--- WebForm/WebForm/Form1.cs
+++ WebForm/WebForm/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using WebForm.Entities;
 using WebForm.MNBServiceReference;
 
@@ -19,7 +20,12 @@ namespace WebForm
         public Form1()
         {
             InitializeComponent();
+            Consume();
+            LoadXml();
+            dataGridView1.DataSource = Rates;
         }
+
+
 
         void Consume()
         {
@@ -31,6 +37,26 @@ namespace WebForm
             var response = mnbService.GetExchangeRates(request);
             string result = response.GetExchangeRatesResult;
             File.WriteAllText("export.xml", result);
+        }private void LoadXml()
+        {
+            XmlDocument xml = new XmlDocument();
+           
+            xml.LoadXml(input);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                 var rate = new RateData();
+                Rates.Add(rate);
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
     }
 }
