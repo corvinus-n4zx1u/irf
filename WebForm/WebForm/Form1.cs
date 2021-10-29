7b9b967a225db2202a1e8d7bc274fad9515463de
Sára Szabó <saraszabo999@gmail.com>
2021-10-29 10:42:21 +0200

    show data on diagram

--- WebForm/WebForm/Form1.cs
+++ WebForm/WebForm/Form1.cs
@@ -8,24 +8,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using WebForm.Entities;
 using WebForm.MNBServiceReference;
 
 namespace WebForm
 {
-    BindingList<RateData> Rates = new BindingList<RateData>();
+    
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
-            Consume();
-            LoadXml();
+            string xmlstring = Consume();
+            LoadXml(xmlstring);
             dataGridView1.DataSource = Rates;
+            Charting();
         }
 
+        private void Charting()
+        {
+            chartRateData.DataSource = Rates;
 
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+
+        }
 
         void Consume()
         {
@@ -37,7 +58,9 @@ namespace WebForm
             var response = mnbService.GetExchangeRates(request);
             string result = response.GetExchangeRatesResult;
             File.WriteAllText("export.xml", result);
-        }private void LoadXml()
+        }
+        
+        private void LoadXml(string input)
         {
             XmlDocument xml = new XmlDocument();
            
