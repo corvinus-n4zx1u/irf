1177c37d7d8cf4d81c077d4233e47b47842b757e
Sára Szabó <saraszabo999@gmail.com>
2021-11-12 14:30:31 +0100

    abstraction in conveyor

--- nyolcadik/nyolcadik/Entities/BallFactory.cs
+++ nyolcadik/nyolcadik/Entities/BallFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using nyolcadik.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace nyolcadik.Entities
 {
-    public class BallFactory
+    public class BallFactory : IToyFactory
     {
         public Ball CreateNew()
         {
