4a0037bbaf6328e3773d3a5322445b5be58fb161
Sára Szabó <saraszabo999@gmail.com>
2021-11-26 10:23:37 +0100

    account control

---
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+
+    }
+}
