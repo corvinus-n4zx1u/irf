3a75001915bfb1b6b335c2c41ab62609bf09f4b9
Sára Szabó <saraszabo999@gmail.com>
2021-11-26 10:35:34 +0100

    email validate

--- UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -2,19 +2,31 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
-        [Test]
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true)
+        ]           
         public void TestValidateEmail(string email, bool expectedResult)
         {
             // Arrange
+            var accountController = new AccountController();
+
 
             // Act
+            var actualResult = accountController.ValidateEmail(email);
 
             // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+            
 
         }
 
