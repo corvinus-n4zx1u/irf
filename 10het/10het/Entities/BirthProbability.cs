60dd80e3369f14a5fae7f225a997a775f17f61f6
Sára Szabó <saraszabo999@gmail.com>
2021-12-03 10:08:00 +0100

    create classes

---
+++ 10het/10het/Entities/BirthProbability.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10het.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        
+        public int NbrOfChildren { get; set; }
+        public double P { get; set; }
+
+        
+    }
+}
