6030e9346e26dd395e59e30e3279685af144e8b7
Sára Szabó <saraszabo999@gmail.com>
2021-12-10 10:13:54 +0100

    build population

--- EvolutionExample/EvolutionExample/Form1.cs
+++ EvolutionExample/EvolutionExample/Form1.cs
@@ -16,6 +16,11 @@ namespace EvolutionExample
         GameController gc = new GameController();
         GameArea ga;
 
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
